3983095cc760db2d52f55fee674c12306a582c0e
solohiddin <sirojiddinovsolohiddin961@gmail.com>
2026-07-25 22:58:01 +0500

    static files

--- root/settings.py
+++ root/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Leo API',
