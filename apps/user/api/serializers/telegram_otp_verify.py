4e492b852561837cf4793106f7cc026696fc98d1
solohiddin <sirojiddinovsolohiddin961@gmail.com>
2026-07-30 14:24:50 +0500

    updated verify serializer

--- apps/user/api/serializers/telegram_otp_verify.py
+++ apps/user/api/serializers/telegram_otp_verify.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
 
 class TelegramOtpVerifySerializer(serializers.Serializer):
-    id = serializers.CharField()
+    token = serializers.CharField()
     otp = serializers.CharField()
