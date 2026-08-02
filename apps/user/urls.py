c6dd35a1ee1c34fbc20cd4288ee715e3bbbe5184
solohiddin <sirojiddinovsolohiddin961@gmail.com>
2026-08-02 11:07:13 +0500

    added url

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from apps.user.api.views.login import LoginAPIView
 from apps.user.api.views.register import RegisterAPIView
+from apps.user.api.views.set_password import SetPasswordAPIView
 from apps.user.api.views.telegram_otp import TelegramOtpView, TelegramWebhookView, TelegramOtpPollView, TelegramOtpVerifyView
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('telegram_otp_poll/', TelegramOtpPollView.as_view(), name='telegram_otp_poll'),
     path('telegram_otp_verify/', TelegramOtpVerifyView.as_view(), name='telegram_otp_verify'),
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('set_password/', SetPasswordAPIView.as_view(), name='set_password'),
 ]
