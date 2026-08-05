8f4ef0332206b223290334e2caa6c26ead3dd63c
solohiddin <sirojiddinovsolohiddin961@gmail.com>
2026-08-05 14:26:50 +0500

    optimized db;

--- root/settings.py
+++ root/settings.py
@@ -81,6 +81,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        "OPTIONS": {
+            "timeout": 20,
+            "init_command": (
+                "PRAGMA journal_mode=WAL;"
+                "PRAGMA synchronous=NORMAL;"
+                "PRAGMA busy_timeout=5000;"
+            ),
+        },
     }
 }
 
