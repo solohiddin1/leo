377d7e6598de4775c356203815fd293038b3fcbb
solohiddin <sirojiddinovsolohiddin961@gmail.com>
2026-08-05 17:35:53 +0500

    fixed bcrypt

--- root/settings.py
+++ root/settings.py
@@ -182,8 +182,7 @@ SIMPLE_JWT = {
 PASSWORD_HASHERS = {
     "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    # "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
 }
 MODELTRANSLATION_DEFAULT_LANGUAGE = 'uz'
 MODELTRANSLATION_LANGUAGES = ('uz', 'ru')
