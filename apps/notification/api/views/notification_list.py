24d1085df185bc3a07ef42c6a9c0f5ca4406bac7
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2026-09-09 12:01:18 +0500

    fixed notifications

--- apps/notification/api/views/notification_list.py
+++ apps/notification/api/views/notification_list.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import GenericAPIView
+from django.db.models import Case, When, IntegerField, Count
 
 from apps.notification.api.serializers.notification import NotificationSerializer
 from apps.notification.repositories.notification_repo import NotificationRepo
@@ -13,6 +14,8 @@ class NotificationListView(GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         notifications = NotificationRepo.get_user_notifications(request.user)
+        unread = notifications.filter(is_read=False).count()
         page = self.paginate_queryset(notifications)
+        self.paginator.extra_data = {'unread': unread}
         serializer = self.get_serializer(page, many=True)
         return self.get_paginated_response(serializer.data)
--- apps/shared/utils/paginator.py
+++ apps/shared/utils/paginator.py
@@ -6,13 +6,18 @@ from apps.shared.utils.utils import success_response
 class CustomPagination(PageNumberPagination):
     page_size = 10
 
+    def __init__(self):
+        extra_data = {}
+
     def get_paginated_response(self, data):
+
         return success_response(
             {
                 'count': self.page.paginator.count,
                 'next': self.get_next_link(),
                 'previous': self.get_previous_link(),
                 'has_next': self.page.has_next(),
+                **self.extra_data,
                 'results': data,
             }
         )
