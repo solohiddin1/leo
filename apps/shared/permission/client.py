cb042778775d22e8c22fe4867ce5a274c4b0f8cb
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2026-09-15 13:03:28 +0500

    fixed permission

--- apps/shared/permission/client.py
+++ apps/shared/permission/client.py
@@ -1,12 +1,9 @@
 from rest_framework.permissions import BasePermission
 
-from apps.user.models import User
-
 
 class ClientPermission(BasePermission):
     def has_permission(self, request, view):
         if not request.user or not request.user.is_authenticated:
             return False
 
-        db_user = User.objects.get(pk=request.user.id)
-        return bool(db_user.is_verified and db_user.is_active)
+        return bool(request.user.is_verified and request.user.is_active)
