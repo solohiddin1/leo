d24fa79caf8f78f50c6c39b6a4f494bbd54367d2
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2026-09-15 15:39:22 +0500

    fixed all stores

--- apps/user/api/views/store.py
+++ apps/user/api/views/store.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework.generics import ListAPIView
 
 from apps.shared.models import Store
@@ -6,6 +7,17 @@ from apps.shared.utils.utils import success_response
 from apps.user.api.serializers.store import StoreSerializer
 
 
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="all_stores",
+            type=bool,
+            location=OpenApiParameter.QUERY,
+            description="Fetch all stores regardless of bonus code claims",
+            required=False,
+        )
+    ]
+)
 class UserRedeemedStoreListView(ListAPIView):
     permission_classes = [ClientPermission]
     serializer_class = StoreSerializer
@@ -14,7 +26,11 @@ class UserRedeemedStoreListView(ListAPIView):
         all_stores = self.request.query_params.get("all_stores", "").lower() == "true"
         if all_stores:
             return Store.objects.all()
-        return Store.objects.filter(bonus_claims__user=self.request.user).distinct()
+
+        user_stores = Store.objects.filter(bonus_claims__user=self.request.user).distinct()
+        if not user_stores.exists():
+            return Store.objects.all()
+        return user_stores
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
